98170e82aff2e9f33c0317ed4494b8673da25780
Collin Jin <collinjin25@gmail.com>
2025-10-26 23:17:08 -0500

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+const { DATABASE_URL } = process.env;
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = { pool };
