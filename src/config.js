6d8295474be941f591d150958744a9f34a510524
Collin Jin <collinjin25@gmail.com>
2025-11-02 22:25:21 -0600

    Initial empty main branch

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-const { DATABASE_URL } = process.env;
-const pool = new Pool({
-  connectionString: DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = { pool };
